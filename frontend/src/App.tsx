1bcac127a6bc84f4872ad8e81548f60ad2a5dfb7
Kadu <ce.magalhaespereira@gmail.com>
2022-11-27 13:11:30 -0300

    Add Hero component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
 import { Header } from './components/Header/Header'
+import { Hero } from './components/Hero/Hero'
 
 function App() {
-  return <Header />
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  )
 }
 
 export default App
