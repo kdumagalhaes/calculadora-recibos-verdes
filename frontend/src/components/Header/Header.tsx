26e99107ee36cdd4de71770440271176475a2c2e
Kadu <ce.magalhaespereira@gmail.com>
2022-11-27 12:34:31 -0300

    Add Header component

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,14 +2,19 @@ import { Emoji } from '../Emoji/Emoji'
 
 export function Header() {
   return (
-    <header className="container mx-auto px-2 flex justify-between h-16 items-center">
-      <h1 className="flex gap-2">
-        Calculadora • Recibos Verdes
+    <header className="container max-w-6xl mx-auto px-2 flex justify-between h-20 items-center">
+      <h1 className="flex gap-2 text-xl">
+        Calculadora de Recibos Verdes
         <Emoji label="money-mouth face" symbol="🤑" />
       </h1>
-      <div className="flex gap-3">
-        <span>kdumagalhaes</span>
-        <a href="#">sair</a>
+      <div className="flex gap-3 items-center">
+        <p className="font-thin">kdumagalhaes</p>
+        <a
+          className="text-emerald-400 py-1 px-2 rounded bg-emerald-700 hover:bg-emerald-900 transition-colors"
+          href="#"
+        >
+          sair
+        </a>
       </div>
     </header>
   )
