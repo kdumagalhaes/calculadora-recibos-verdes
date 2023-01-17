95ac51ad487ab7fb3298667c8c5dec3e843f6dac
Kadu <ce.magalhaespereira@gmail.com>
2023-01-17 18:00:38 -0300

    Remove logout button

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -5,15 +5,6 @@ export function Header() {
   return (
     <header className="container max-w-6xl mx-auto px-2 flex justify-between h-20 items-end max-[980px]:p-4">
       <Logo />
-      <div className="flex gap-3 items-center">
-        <p className="font-thin">kdumagalhaes</p>
-        <a
-          className="text-emerald-400 py-1 px-2 rounded bg-emerald-700 hover:bg-emerald-900 transition-colors"
-          href="#"
-        >
-          Sair
-        </a>
-      </div>
     </header>
   )
 }
